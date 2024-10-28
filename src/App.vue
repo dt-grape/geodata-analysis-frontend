e6e9e0e19e7fe8de8fde7c0edc290dda66eb5cd6
grapedt <dimon555kz21@gmail.com>
2024-10-28 17:05:24 +0700

    added files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {Button} from '@/components/ui/button'
+
+</script>
+
+<template>
+
+  <div class="w-screen h-screen flex items-center justify-center">
+    <Button>Click me</Button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
