9c4db7295c59d771d6a8c8f78fcf4b3f042864fd
grapedt <dimon555kz21@gmail.com>
2024-10-29 10:58:10 +0700

    feat: added get method

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <script setup>
-import {Button} from '@/components/ui/button'
+import L from "leaflet";
+import { onMounted, ref, toRaw } from "vue";
 
+import "./assets/leaflet.css";
+import axios from "./axios.js";
+
+const map = ref(null);
+const currentLatLng = ref(null);
+const loadObjects = async () => {
+  const response = await axios.get("/");
+  console.log(response.data);
+
+  const objects = response.data;
+
+  objects.forEach((object) => {
+    L.marker([object.latitude, object.longitude])
+      .addTo(toRaw(map.value))
+      .bindPopup(object.name)
+      .openPopup();
+  });
+};
+
+onMounted(() => {
+  map.value = L.map("map").setView([54.986392, 82.862919], 20);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+  }).addTo(map.value);
+
+  map.value.on("click", onMapClick);
+
+  loadObjects();
+});
+
+const onMapClick = (e) => {
+  currentLatLng.value = e.latlng;
+  console.log(currentLatLng.value);
+};
 </script>
 
 <template>
-
-  <div class="w-screen h-screen flex items-center justify-center">
-    <Button>Click me</Button>
+  <div class="flex w-screen h-screen items-center justify-center">
+    <div class="bg-gray rounded-xl w-[1200px] h-[800px]">
+      <div id="map"></div>
+    </div>
   </div>
-
 </template>
 
 <style scoped>
-
+#map {
+  height: 100%;
+  width: 100%;
+}
 </style>
--- src/axios.js
+++ src/axios.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "http://api.geodata.novichikhin.com/",
 });
+
+export default instance;
